4dd59b888209e29ab4de8a600be61dd783463e39
Tshikovha28 <vhenganiem28@gmail.com>
2024-03-11 07:43:02 -0700

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,16 +1,17 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
- * swap - Swap two integers in an array.
+ * swap_ints - Swap two integers in an array.
  * @a: The first integer to swap.
  * @b: The second integer to swap.
  */
-void swap(int *a, int *b)
+void swap_ints(int *a, int *b)
 {
-int tmp = *a;
-*a = *b;
-*b = tmp;
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -18,37 +19,28 @@ int tmp = *a;
  * @array: An array of integers to sort.
  * @size: The size of the array.
  *
- * Description: Print the array after each swap.
+ * Description: Prints the array after each swap.
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t i, len = size;
-int bubbly = 0;
+	size_t i, len = size;
+	bool bubbly = false;
 
-if (array == NULL || size < 2)
-return;
+	if (array == NULL || size < 2)
+		return;
 
-while (bubbly == 0)
-{
-bubbly = 1;
-for (i = 0; i < len - 1; i++)
-{
-if (array[i] > array[i + 1])
-{
-swap(array + i, array + i + 1);
-
-/*Print the array after each swap*/
-printf("Swap: %d <--> %d\n", array[i], array[i + 1]);
-for (size_t k = 0; k < size; k++)
-{
-printf("%d ", array[k]);
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len--;
+	}
 }
-printf("\n");
-
-bubbly = 0;
-}
-}
-len--;
-}
-}
-
